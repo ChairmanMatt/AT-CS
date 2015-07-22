5fdaf7a88b63e328872238fa2ae9d0d69bc9aeec
Matt Lu <Matt Lu@192.168.0.22>
2015-07-22 13:52:49 -0700

    hue

--- Screwing around/Source/threads/Hue.java
+++ Screwing around/Source/threads/Hue.java
@@ -4,11 +4,23 @@ import java.util.ArrayList;
 
 public class Hue implements Runnable{
 
+	private Thread t;
+	private String threadName;
 	ArrayList<String> str;
-	
-	public Hue(){
+
+	public Hue(String name){
+		threadName = name;
 		str = new ArrayList<String>();
 	}
+
+	public void start(){
+		if (t == null)
+		{
+			t = new Thread (this, threadName);
+			t.start ();
+		}
+	}
+
 	public void run() {
 		// TODO Auto-generated method stub
 		for(int x = 0; x<100; x++){
@@ -17,6 +29,6 @@ public class Hue implements Runnable{
 		}
 	}
 
-	
-	
+
+
 }
--- Screwing around/Source/threads/Tester.java
+++ Screwing around/Source/threads/Tester.java
@@ -3,8 +3,8 @@ package threads;
 public class Tester {
 
 	public static void main(String[] args){
-		Hue h1 = new Hue();
-		Hue h2 = new Hue();
+		Hue h1 = new Hue("One");
+		Hue h2 = new Hue("Two");
 		Thread t1 = new Thread(h1);
 		Thread t2 = new Thread(h2);
 		t1.start();
