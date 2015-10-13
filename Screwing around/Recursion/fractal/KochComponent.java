d74a036309f436b3556fd2d59c89e4300f642c5b
16ML0369 <16ML0369@HSS-101-03.westwindsor.k12.nj.us>
2015-10-13 12:43:09 -0400

    things

--- Screwing around/Recursion/fractal/KochComponent.java
+++ Screwing around/Recursion/fractal/KochComponent.java
@@ -38,36 +38,6 @@ public class KochComponent extends JComponent
 		length = aLength;
 	}
 
-
-
-	public void paintComponent(Graphics g)
-	{
-		Graphics2D g2 = (Graphics2D) g;
-
-		KochLine koch = new KochLine();
-
-		int x1 = 10;
-
-		int y1 = length / 2;
-
-		int x2 = x1 + length;
-
-		int y2 = y1;
-
-		int x3 = x1 + (length / 2);
-
-		int y3 = (y1 +length);
-
-		koch.draw(g2, numIterations, x1, y1, x2, y2);
-
-		koch.draw(g2, numIterations, x2, y2, x3, y3);
-
-		koch.draw(g2, numIterations, x3, y3, x1, y1);
-
-	}
-
-
-
 	public static void main(String[] args)
 	{
 		JFrame frame = new JFrame();
@@ -78,7 +48,7 @@ public class KochComponent extends JComponent
 
 		frame.setSize(FRAME_WIDTH, FRAME_HEIGHT);
 
-		frame.setTitle("Koch Snowflake");
+		frame.setTitle("My Rectangle (s)");
 
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 
@@ -90,11 +60,40 @@ public class KochComponent extends JComponent
 
 		int iterations = Integer.parseInt(input);
 
-		KochComponent component = new KochComponent(iterations, 300);
+		KochComponent component = new KochComponent(iterations, 100);
 
 		frame.add(component);
 
 		frame.setVisible(true);
+
+	}
+
+	public void paintComponent(Graphics g)
+	{
+		Graphics2D g2 = (Graphics2D) g;
+
+		KochLine koch = new KochLine();
+
+		int x1 = 150;
+		int y1 = 150;
+
+		int x2 = x1 + length;
+		int y2 = y1;
+
+		int x3 = x2;
+		int y3 = y2 - length;
+		
+		int x4 = x1;
+		int y4 = y3;
+		
+
+		koch.draw(g2, numIterations, x1, y1, x2, y2);
+
+		koch.draw(g2, numIterations, x2, y2, x3, y3);
+
+		koch.draw(g2, numIterations, x3, y3, x4, y4);
+		
+		koch.draw(g2, numIterations, x4, y4, x1, y1);
 
 	}
 
--- Screwing around/Recursion/fractal/KochLine.java
+++ Screwing around/Recursion/fractal/KochLine.java
@@ -9,50 +9,113 @@ public class KochLine
 
 {
 
-   public void draw(Graphics2D g2, int iteration, double x1, double y1, double x2, double y2)
+	public void draw(Graphics2D g2, int iteration, double x1, double y1, double x2, double y2)
 
-   {
+	{
 
-      if (iteration == 1)
+		if (iteration == 1)
 
-      {
+		{
 
-         g2.draw(new Line2D.Double(x1, y1, x2, y2));
+			g2.draw(new Line2D.Double(x1, y1, x2, y2));
 
-      }
+		}
 
-      else
+		else
 
-      {
+		{
+			//difference between points
+			double dx = Math.abs((x2 - x1) / 3);
 
-         double angle = 60 * Math.PI / 180; // 60 degrees
+			double dy = Math.abs((y2 - y1) / 3);
 
-         double dx = (x2 - x1) / 3;
+			//whichever one is nonzero
+			if(dx == 0)
+				dx = dy;
+			else dy = dx;
+				
+			//points
+			double x5,y5;
+			double x6,y6;
+			double x7,y7;
+			double x8,y8;
 
-         double dy = (y2 - y1) / 3;
+			
+			//top of rectangle
+			if(y1 == y2 && x1 < x2){
+				x5 = x1 + dx;
+				y5 = y1;
 
-         double x3 = x1 + dx;
+				x6 = x5;
+				y6 = y5 - dy;
 
-         double y3 = y1 + dy;
+				x7 = x6 + dx;
+				y7 = y6;
 
-         double x4 = x2 - dx;
+				x8 = x7;
+				y8 = y7 + dy;
+			}
+			
+			//bottom of rectangle
+			else if(y1 == y2){
+				x5 = x1 - dx;
+				y5 = y1;
 
-         double y4 = y2 - dy;
+				x6 = x5;
+				y6 = y5 + dy;
 
-         double x5 = x3 + dx * Math.cos(angle) + dy * Math.sin(angle);
+				x7 = x6 - dx;
+				y7 = y6;
 
-         double y5 = y3 + dy * Math.cos(angle) - dx * Math.sin(angle);
+				x8 = x7;
+				y8 = y7 - dy;
+			}
+			
+			//right side of rectangle
+			else if(x1 == x2 && y1 < y2){
+				x5 = x1;
+				y5 = y1 + dy;
 
-         draw(g2, iteration - 1, x1, y1, x3, y3 );
+				x6 = x5 + dx;
+				y6 = y5;
 
-         draw(g2, iteration - 1, x3, y3, x5, y5);
+				x7 = x6;
+				y7 = y6 + dy;
 
-         draw(g2, iteration - 1, x5, y5, x4, y4);
+				x8 = x7 - dx;
+				y8 = y7;
+			}
+			
+			//left side of rectangle
+			else{
+				x5 = x1;
+				y5 = y1 - dy;
 
-         draw(g2, iteration - 1, x4, y4, x2, y2);        
+				x6 = x5 - dx;
+				y6 = y5;
 
-      }
+				x7 = x6;
+				y7 = y6 - dy;
 
-   }
+				x8 = x7 + dx;
+				y8 = y7;
+			}
+			
+			
+			
+
+			draw(g2, iteration - 1, x1, y1, x5, y5);
+
+			draw(g2, iteration - 1, x5, y5, x6, y6);
+
+			draw(g2, iteration - 1, x6, y6, x7, y7);
+
+			draw(g2, iteration - 1, x7, y7, x8, y8);
+
+			draw(g2, iteration - 1, x8, y8, x2, y2);        
+
+		}
+
+	}
 
 }
