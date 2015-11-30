78b3b43c088e5f50d7f4795d852ac79d039f972a
16ml0369 <16ml0369@HSS-101-03.westwindsor.k12.nj.us>
2015-11-30 13:55:26 -0500

    print method reworked

--- Screwing around/Stacks/introduction/CDStack.java
+++ Screwing around/Stacks/introduction/CDStack.java
@@ -5,11 +5,53 @@ import java.util.Stack;
 
 public class CDStack {
 	
-	public static void main(String[] args) {
-		//insert code here
-		Stack <String> myStack;
+	private Stack<String> myStack;
+	
+	public CDStack()
+	{
+		myStack = new Stack<String>();
 		
-
+		myStack.push("Journey");
+		myStack.push("Iagy Pop");
+		myStack.push("Hall & Oats");
+		myStack.push("Genesis");
+		myStack.push("Foreigner");
+		myStack.push("Eagles");
+		myStack.push("Deep Purple");
+		myStack.push("Cheap Trick");
+		myStack.push("Beatles");
+		myStack.push("Aerosmith");
 	}
 
+	/**
+	 * Returns contents of second element of myStack without removing first element
+	 */
+	public String nextCD()
+	{
+		return myStack.peek();
+	}
+	
+	/**
+	 * Returns contents of second element of myStack without removing first element
+	 */
+	public String playNextCD()
+	{
+		return myStack.pop();
+	}
+	
+	public Stack<String> getStack(){
+		return myStack;
+	}
+	
+	public void printCD()
+	{
+		while(! myStack.isEmpty())
+			System.out.println(myStack.pop()+"");
+	}
+	
+	public static void main(String[] args){
+		CDStack cds = new CDStack();
+		cds.printCD();
+	}
+	
 }
