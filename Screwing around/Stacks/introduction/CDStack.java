206a2cce2518b3283dcc92b50dc4fa14887a7aee
16ml0369 <16ml0369@HSS-101-03.westwindsor.k12.nj.us>
2015-11-30 14:44:02 -0500

    binary still to be completed

--- Screwing around/Stacks/introduction/CDStack.java
+++ Screwing around/Stacks/introduction/CDStack.java
@@ -4,13 +4,13 @@ import java.util.Iterator;
 import java.util.Stack;
 
 public class CDStack {
-	
+
 	private Stack<String> myStack;
-	
+
 	public CDStack()
 	{
 		myStack = new Stack<String>();
-		
+
 		myStack.push("Journey");
 		myStack.push("Iagy Pop");
 		myStack.push("Hall & Oats");
@@ -30,7 +30,7 @@ public class CDStack {
 	{
 		return myStack.peek();
 	}
-	
+
 	/**
 	 * Returns contents of second element of myStack without removing first element
 	 */
@@ -38,20 +38,74 @@ public class CDStack {
 	{
 		return myStack.pop();
 	}
-	
+
 	public Stack<String> getStack(){
 		return myStack;
 	}
-	
+
 	public void printCD()
 	{
 		while(! myStack.isEmpty())
 			System.out.println(myStack.pop()+"");
 	}
+
+	public void printCDs2()
+	{
+		Iterator<String> i = myStack.iterator();
+		while(i.hasNext())
+			System.out.println(i.next());
+	}
+
+	public void printCDs3()
+	{
+		for(String s:myStack)
+			System.out.println(s);
+	}
+
+	public void printCDRecursive()
+	{
+		if(myStack.isEmpty())
+			return;
+		System.out.println(myStack.pop());
+		printCDRecursive();
+	}
+
+	public Stack<String> reverseStack(Stack<String> stack){
+		Stack<String> temp = new Stack<String>();
+		while(!stack.isEmpty()){
+			temp.push((String) stack.pop());
+		}
+
+		return temp;
+	}
+
+	public void addOnTop(Stack<String> stack){
+		stack = reverseStack(stack);
+		while(!stack.isEmpty()){
+			myStack.push((String) stack.pop());
+		}
+	}
+
+	public void binaryThing(int num, boolean wasModded){
+		if(num <= 1)
+			return;
+		
+		if(wasModded = false){
+			myStack.push((num % 2)+" ");
+			num = num % 2;
+			binaryThing(num, true);
+		}
+		else{
+			num = num / 2;
+			binaryThing(num, false);
+		}
+		
+	}
 	
 	public static void main(String[] args){
 		CDStack cds = new CDStack();
+		cds.binaryThing(34, false);
 		cds.printCD();
 	}
-	
+
 }
