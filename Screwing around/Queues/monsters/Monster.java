a3bf6c18e1ca6cacf48b676422748ce9b6efdc02
16ml0369 <16ml0369@HSS-101-17.westwindsor.k12.nj.us>
2016-01-08 12:02:54 -0500

    starting

---
+++ Screwing around/Queues/monsters/Monster.java
@@ -0,0 +1,82 @@
+package monsters;
+
+
+public class Monster implements Comparable<Monster> {
+
+	private int height;
+	private int weight;
+	private int age;
+
+	public Monster(){
+		height = 1;
+		weight = 1;
+		age = 1;
+	}
+
+	public Monster(int ht, int wt, int ag)  {
+		height = ht;
+		weight = wt;
+		age = ag;
+	}
+
+	public void setWeight(int wt) {
+		weight = wt;
+	}
+
+	public void setHeight( int ht) {
+		height = ht;
+	}
+
+	public void setAge( int ag) {
+		age = ag;
+	}
+
+	public Object clone() {
+		return new Monster(height, weight, age);
+	}
+
+	public int getHeight() {
+		return height;
+	}
+	
+	public int getWeight() {
+		return weight;
+	}
+
+	public int getAge() {
+		return age;
+	}
+
+	public boolean equals(Object o) {
+		if(height != ((Monster) o).getHeight())
+			return false;
+
+		if(weight != ((Monster) o).getWeight())
+			return false;
+
+		if(age != ((Monster) o).getAge())
+			return false;
+
+		return true;
+	}
+
+	public int compareTo(Monster rhs) {
+		if(height != ((Monster) rhs).getHeight())
+			return height-((Monster) rhs).getHeight();
+
+		if(weight != ((Monster) rhs).getWeight())
+			return weight-((Monster) rhs).getWeight();
+
+		if(age != ((Monster) rhs).getAge())
+			return age-((Monster) rhs).getAge();
+
+		return 0;
+	}
+
+
+
+	public String toString( ) {
+		return "Height " + height + " Weight " + weight + " Age " + age;
+	}
+
+}
---
+++ Screwing around/Queues/monsters/MonsterPQRunner.java
@@ -0,0 +1,9 @@
+package monsters;
+
+public class MonsterPQRunner
+{
+	public static void main ( String[] args )
+	{
+		//add test cases			
+	}
+}
