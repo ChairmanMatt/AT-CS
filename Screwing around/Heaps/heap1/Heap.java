44044f36e2c53f01689ceae298d1912aa1d54e0f
Sir Matthew Lu <CMS-177116@hotmail.com>
2016-04-08 13:43:32 -0400

    swapDown not working (remove does not autosort)

--- Screwing around/Heaps/heap1/Heap.java
+++ Screwing around/Heaps/heap1/Heap.java
@@ -9,6 +9,8 @@ public class Heap
 	public Heap()
 	{
 		list = new ArrayList<Integer>();
+		list.add(null);
+		System.out.println(list);
 	}
 
 	public void add(int value)
@@ -19,31 +21,71 @@ public class Heap
 
 	public void swapUp(int bot)
 	{
+		if(bot <= 1)
+			return;
 		
+		if(list.get(bot/2) < list.get(bot)){
+			swap(bot, bot/2);
+		}
 		
+		swapUp(bot/2);
 	}
 
 	public void remove( )
 	{
-		list.set(0,list.get(list.size()-1));
+		list.set(1,list.get(list.size()-1));
 		list.remove(list.size()-1);
-		swapDown(list.size());
+		swapDown(list.get(1));
 	}
 
 	public void swapDown(int top)
 	{
+		//base
+		if(top*2 >= list.size())
+			return;
+		
+		System.out.print("SD");
+		
+		if(list.get(top*2) > list.get(top)){
+			swap(top*2, top);
+			swapDown(top*2);
+		}
+		
+		else if(list.get(top*2 + 1) > list.get(top)){
+			swap(top*2 + 1, top);
+			swapDown(top*2+1);
+		}
+		
+		
 	}
 
 	private void swap(int start, int finish)
-	{	
+	{
+		int temp = list.get(finish);
+		
+		list.set(finish, list.get(start));
+		list.set(start,  temp);
 	}
 
 	public void print()
 	{
 		System.out.println("\n\nPRINTING THE HEAP!\n\n");
-		System.out.println();
+//		System.out.println(print(1, ""));
+		System.out.println(toString());
 	}
 
+	public String print(int ind, String s){
+		if(ind >= list.size())
+			return s+"";
+		
+		s+=list.get(ind)+" ";
+		if(ind%2 != 0)
+			s+="\n";
+		
+		
+		return(print(ind+1, s));
+	}
+	
 	public String toString()
 	{
 		return list.toString();
